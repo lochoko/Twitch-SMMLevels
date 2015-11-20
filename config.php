d7c1c0cbf379eaebb9cf61a4173064ea8a5d380c
lochoko <vlin@lochoko.com>
2015-11-20 16:47:43 -0500

    Update config.php
    Simplified Config page

--- config.php
+++ config.php
@@ -2,4 +2,8 @@
 
 header("Access-Control-Allow-Origin: *");
 
-$db = new PDO('mysql:host=localhost;dbname=<DB Name>;charset=utf8', '<USERNAME>', '<PASSWORD>');
+$dbname = ''; // Enter the Database Name here
+$username = ''; // Enter the username to log into MySQL here
+$password = ''; // Enter the password to log into MySQL here
+
+$db = new PDO('mysql:host=localhost;dbname='.$dbname.';charset=utf8', $username, $password);
